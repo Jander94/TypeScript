79f9196bc3b1f298faf2a04d860f515d8d195ddf
Jander Gustavo <jander.gustavo@gmail.com>
2022-12-05 20:33:01 -0300

    Iniciando curso de TypeScript

--- 6_OBJECT_TYPES/index.js
+++ 6_OBJECT_TYPES/index.js
@@ -11,3 +11,27 @@ const shirt = {
     isAvailable: true,
 };
 showProductDetails(shirt);
+showProductDetails({ name: 'tenis', price: 30.99, isAvailable: false });
+function showUserDetails(user) {
+    console.log(`E-mail do usuário: ${user.email}`);
+    if (user.role) {
+        console.log(`Sua função no sistema é de: ${user.role}`);
+    }
+}
+const u1 = { email: 'matheus@teste.com', role: 'Admin' };
+const u2 = { email: 'joao@teste.com' };
+showUserDetails(u1);
+showUserDetails(u2);
+const fusca = {
+    brand: 'Vw',
+    wheels: 4,
+};
+console.log(fusca);
+let coords = {
+    x: 10,
+};
+coords.y = 15;
+console.log(coords);
+const arr1 = [1, 2, 3];
+// const arr2: OnlyNumberArray = ['1', '2', '3']
+console.log(arr1);
--- 6_OBJECT_TYPES/index.ts
+++ 6_OBJECT_TYPES/index.ts
@@ -1,4 +1,4 @@
-// 1 - tipo de objeto para funções
+                              // 1 - tipo de objeto para funções
 interface Product {
   name: string
   price: number
@@ -21,3 +21,62 @@ const shirt: Product = {
 }
 
 showProductDetails(shirt)
+showProductDetails({name: 'tenis', price: 30.99, isAvailable: false})
+
+
+                              // 2 - propriedade opcional em interface
+interface User {
+  email: string
+  role?: string
+}
+
+function showUserDetails(user: User) {
+  console.log(`E-mail do usuário: ${user.email}`)
+  if (user.role) {
+    console.log(`Sua função no sistema é de: ${user.role}`)
+  }
+}
+
+const u1 = { email: 'matheus@teste.com', role: 'Admin' }
+const u2 = { email: 'joao@teste.com' }
+
+showUserDetails(u1)
+showUserDetails(u2)
+
+                              // 3 - readonly
+interface Car {
+  brand: string
+  readonly wheels: number
+}
+
+const fusca: Car = {
+  brand: 'Vw',
+  wheels: 4,
+}
+
+console.log(fusca)
+
+// fusca.wheels = 5
+
+                              // 4 - index signature
+
+interface CoordObject {
+  [index: string]: number
+}
+
+let coords: CoordObject = {
+  x: 10,
+}
+
+coords.y = 15
+
+console.log(coords)
+
+interface OnlyNumberArray {
+  [index: number]: number
+}
+
+const arr1: OnlyNumberArray = [1, 2, 3]
+// const arr2: OnlyNumberArray = ['1', '2', '3']
+
+console.log(arr1)
